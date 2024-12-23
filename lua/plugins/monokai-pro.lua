5cbba86bd3fdf0736e6d65e9fdcddaac8cb8e153
wabox324@gmail.com <wabox324@gmail.com>
2024-12-22 23:10:06 -0500

    se agrega archivo de configuracion de alacritty

--- lua/plugins/monokai-pro.lua
+++ lua/plugins/monokai-pro.lua
@@ -15,8 +15,10 @@ return {
   {
     "loctvl842/monokai-pro.nvim",
     init = function()
-    vim.cmd.colorscheme "monokai-pro"
-    vim.cmd.MonokaiPro_Theme = "spectrum"
+    vim.cmd.colorscheme "monokai-pro-spectrum"
+    vim.cmd.MonakaiPro_Theme = "monokai-pro"
+    vim.cmd.MonokaiPro = "spectrum"
+    vim.cmd.MonokaiPro_TransparentBackground = true
     vim.cmd.transparent_background = true
     end,
   }, 
